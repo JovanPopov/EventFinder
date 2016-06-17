58fdb2d4cc178b7367dc5a9d6b0b381643974309
JovanPopov <jovan.popov.88@gmail.com>
2016-06-17 21:28:25 +0200

    app launching splash v2

--- app/src/main/java/ftn/eventfinder/activities/Splash.java
+++ app/src/main/java/ftn/eventfinder/activities/Splash.java
@@ -25,7 +25,7 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        //setContentView(R.layout.activity_splash);
 
         startService(new Intent(this, ClearDbService.class));
 
